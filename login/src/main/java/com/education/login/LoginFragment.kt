5f79f0d1c2b1dc5031ccf8ef253c0a60d80136b4
Nikita Kononenko (intern) <nekitkononenko@rambler.ru>
2020-03-09 11:19:01 +0300

    Закончил с версткой экрана с авторизацией Перехожу к реализации валидации пользовательских данных

--- login/src/main/java/com/education/login/LoginFragment.kt
+++ login/src/main/java/com/education/login/LoginFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.viewModels
 import com.education.login.viewmodel.LoginViewModel
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginFragment : Fragment() {
@@ -15,7 +17,10 @@ class LoginFragment : Fragment() {
         fun newInstance() = LoginFragment()
     }
 
-    private lateinit var viewModel: LoginViewModel
+    private val viewModel: LoginViewModel by viewModels()
+    private lateinit var enterBtn: Button
+    private lateinit var loginTextInput: TextInputLayout
+    private lateinit var passwdTextInput: TextInputLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +31,19 @@ class LoginFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        //viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewElements(view)
+    }
+
+    private fun initViewElements(view: View) {
+        enterBtn = view.findViewById(R.id.enterButton)
+        enterBtn.setBackgroundColor(resources.getColor(R.color.fill_button_color))
+        loginTextInput = view.findViewById(R.id.loginTextInput)
+        passwdTextInput = view.findViewById(R.id.passwdTextInput)
+    }
 }
