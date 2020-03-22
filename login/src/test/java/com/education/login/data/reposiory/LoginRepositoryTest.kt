364e18787d0c3c698829d677997d8e92a4766c00
Nikita Kononenko (intern) <nekitkononenko@rambler.ru>
2020-03-22 22:47:27 +0300

    - Сделал рефакторинг LoginRepositoryTest

--- login/src/test/java/com/education/login/data/reposiory/LoginRepositoryTest.kt
+++ login/src/test/java/com/education/login/data/reposiory/LoginRepositoryTest.kt
@@ -12,8 +12,6 @@ import com.education.login.expires_at
 import com.education.login.request_token
 import com.education.login.session_id
 import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
 import io.reactivex.Single.just
 import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.TestScheduler
@@ -25,32 +23,27 @@ import java.util.concurrent.TimeUnit
 object LoginRepositoryTest : Spek({
     Feature("Check login(user: User) method") {
         val testScheduler = TestScheduler()
+        val mockTmdbAuthApi: TmdbAuthApi = Mockito.mock(TmdbAuthApi::class.java).also {
+            Mockito.`when`(it.createRequestToken()).thenReturn(just(
+                RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
+            ))
+            Mockito.`when`(it.validateRequestTokenWithLogin(any())).thenReturn(just(
+                RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
+            ))
+        }
+        val mockLocalDataSource: LocalDataSource = Mockito.mock(LocalDataSource::class.java)
+
         Scenario("Check login result if create session true") {
-
-            var mockTmdbAuthApi: TmdbAuthApi
-
-            var mockLocalDataSource: LocalDataSource
 
             var loginRepository: LoginRepository? = null
 
             var testObserver: TestObserver<Void>? = null
 
             Given("Create success data flow from TmdbAuthApi by mocking") {
-                mockTmdbAuthApi = mock {
-                    on { createRequestToken() } doReturn(just(
-                        RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
-                    ))
+                Mockito.`when`(mockTmdbAuthApi.createSessionId(any())).thenReturn(just(
+                    Session(success = true, sessionId = session_id)
+                ))
 
-                    on { validateRequestTokenWithLogin(any()) } doReturn(just(
-                        RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
-                    ))
-
-                    on { createSessionId(any()) } doReturn(just(
-                        Session(success = true, sessionId = session_id)
-                    ))
-                }
-
-                mockLocalDataSource = Mockito.mock(LocalDataSource::class.java)
 
                 loginRepository = LoginRepositoryImpl(mockTmdbAuthApi, mockLocalDataSource)
             }
@@ -71,31 +64,14 @@ object LoginRepositoryTest : Spek({
         }
 
         Scenario("Check login result if create session false") {
-
-            var mockTmdbAuthApi: TmdbAuthApi
-
-            var mockLocalDataSource: LocalDataSource
-
             var loginRepository: LoginRepository? = null
 
             var testObserver: TestObserver<Void>? = null
 
             Given("Create data flow from TmdbAuthApi by mocking with false session") {
-                mockTmdbAuthApi = mock {
-                    on { createRequestToken() } doReturn(just(
-                        RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
-                    ))
-
-                    on { validateRequestTokenWithLogin(any()) } doReturn(just(
-                        RequestToken(success = true, expiresAt = expires_at, requestToken = request_token)
-                    ))
-
-                    on { createSessionId(any()) } doReturn(just(
-                        Session(success = false, sessionId = session_id)
-                    ))
-                }
-
-                mockLocalDataSource = Mockito.mock(LocalDataSource::class.java)
+                Mockito.`when`(mockTmdbAuthApi.createSessionId(any())).thenReturn(just(
+                    Session(success = false, sessionId = "")
+                ))
 
                 loginRepository = LoginRepositoryImpl(mockTmdbAuthApi, mockLocalDataSource)
             }
